5ce95326f35304d376b20a755d7f954893dfd98b
sandip.limbachiya <sandip2451984@gmail.com>
2013-07-19 16:23:50 +0530

    Change in init.js

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -11,21 +11,6 @@ $(document).ready(function() {
 		// go to the first view: mainPage
 	 var mainPageView = getView("mainPage");
   
-    
-    $fh.act({
-  "act": "sayHello",
-  // my cloud function name to call
-  "req": {
-    "key": "Test" // send this value to the cloud
-  }
-}, function(res) {
-  // Cloud call was successful. Alert the response
-  alert('Got response from cloud:' + JSON.stringify(res));
-}, function(msg, err) {
-  // An error occured during the cloud call. Alert some debugging information
-  alert('Cloud call failed with error:' + msg + '. Error properties:' + JSON.stringify(err));
-});
-
 	 sync = $fh.sync;
 
     register.init();
