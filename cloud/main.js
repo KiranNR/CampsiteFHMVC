7274339ef590bc97394fc4c9199a9ff4c9c15032
sandip.limbachiya <sandip2451984@gmail.com>
2013-07-23 16:20:04 +0530

    change code

--- cloud/main.js
+++ cloud/main.js
@@ -51,10 +51,3 @@ sync.init(dataset_id, {}, function() {
   sync.removeCollision(dataset_id, dataHandler.removeCollision);
 });
 
-exports.getConfig = function(params, callback) {
-  console.log("In getConfig() call");
-  var cfg = require("config.js");
-  return callback(null, {config: cfg.config});
-};
-  
-};
