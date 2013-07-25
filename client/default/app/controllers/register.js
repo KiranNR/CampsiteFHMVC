3a22658fdfce9f01036ebfbd8ed91c899be5f5c4
sandip.limbachiya <sandip2451984@gmail.com>
2013-07-25 16:43:25 +0530

    chjhj

--- client/default/app/controllers/register.js
+++ client/default/app/controllers/register.js
@@ -172,9 +172,21 @@ init: function(res,resU) {
             
       var firstName = document.getElementById('first_name').value;
       var lastName = document.getElementById('last_name').value;
+      var address = document.getElementById('address').value;
+      var city = document.getElementById('city').value;
+      var state = document.getElementById('state').value;
+      var zipcode = document.getElementById('zipcode').value;
+      var country = document.getElementById('country').value;
+      var job_title = document.getElementById('job_title').value;
+      var company = document.getElementById('company').value;
+            
       var website = document.getElementById('website').value;
       var blog = document.getElementById('blog').value;
       alert('call Submit Function'+firstName);
+      if(address == ''){
+        alert('Please add address');
+        return false;
+      }
       
       $fh.act({
                 "act": "insertUser",
@@ -182,6 +194,14 @@ init: function(res,resU) {
                 "req": {
                   "first_name": firstName, // send this value to the cloud
                   "last_name" : lastName,
+                  "address" : address,
+                  "city" : city,
+                  "state" : state,
+                  "zipcode" : zipcode, 
+                  "country" : country,
+                  "job_title" : job_title,
+                  "company" : company,
+                  
                   "website" : website,
                   "blog":blog
                 }
