f50904b7d7091b50e3be2de97f5556c232398547
sandip.limbachiya <sandip2451984@gmail.com>
2013-07-29 17:09:46 +0530

    sds

--- client/default/app/controllers/register.js
+++ client/default/app/controllers/register.js
@@ -143,7 +143,7 @@ eventList: function(resEvent) {
       
       var website =   document.getElementById('website').value;
       var blog =      document.getElementById('blog').value;
-      alert(" -->"+firstName+":"+lastName+":"+address+":"+city+":"+state+":"+zipcode+":"+country+":"+job_title+":"+company+":"+website+":"+blog);
+      //alert(" -->"+firstName+":"+lastName+":"+address+":"+city+":"+state+":"+zipcode+":"+country+":"+job_title+":"+company+":"+website+":"+blog);
       
       //alert('call Submit Function'+firstName);
       var tempval = 0;
